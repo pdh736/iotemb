f0ed645051724caa10065ec3149b14a023383c45
Daehee <pdh736@naver.com>
2018-04-18 14:04:26 +0900

    2018-04-18

--- DialClient/GeneratedFiles/ui_dialcient.h
+++ DialClient/GeneratedFiles/ui_dialcient.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'dialcient.ui'
 **
-** Created: Wed Apr 18 12:24:51 2018
+** Created: Wed Apr 18 12:58:58 2018
 **      by: Qt User Interface Compiler version 4.8.0
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
@@ -31,20 +31,21 @@ QT_BEGIN_NAMESPACE
 class Ui_DialCient
 {
 public:
-    QWidget *verticalLayoutWidget;
+    QVBoxLayout *verticalLayout_4;
     QVBoxLayout *verticalLayout;
     QHBoxLayout *horizontalLayout;
     QPushButton *pTimerButton;
     QPushButton *pQuitButton;
     QHBoxLayout *horizontalLayout_2;
     QLabel *pLabel;
-    QSpacerItem *horizontalSpacer;
     QComboBox *pComboBox;
     QHBoxLayout *horizontalLayout_3;
+    QVBoxLayout *verticalLayout_3;
     QDial *pDial;
     QVBoxLayout *verticalLayout_2;
     QPushButton *pArdLedBtn;
     QPushButton *pushButton_2;
+    QSpacerItem *verticalSpacer;
     QHBoxLayout *horizontalLayout_4;
     QProgressBar *pProgressBar;
     QLCDNumber *pLcdNumber;
@@ -53,31 +54,30 @@ public:
     {
         if (DialCient->objectName().isEmpty())
             DialCient->setObjectName(QString::fromUtf8("DialCient"));
-        DialCient->resize(479, 387);
-        verticalLayoutWidget = new QWidget(DialCient);
-        verticalLayoutWidget->setObjectName(QString::fromUtf8("verticalLayoutWidget"));
-        verticalLayoutWidget->setGeometry(QRect(19, 19, 441, 351));
-        verticalLayout = new QVBoxLayout(verticalLayoutWidget);
+        DialCient->resize(490, 641);
+        verticalLayout_4 = new QVBoxLayout(DialCient);
+        verticalLayout_4->setSpacing(6);
+        verticalLayout_4->setContentsMargins(11, 11, 11, 11);
+        verticalLayout_4->setObjectName(QString::fromUtf8("verticalLayout_4"));
+        verticalLayout = new QVBoxLayout();
         verticalLayout->setSpacing(6);
-        verticalLayout->setContentsMargins(11, 11, 11, 11);
         verticalLayout->setObjectName(QString::fromUtf8("verticalLayout"));
-        verticalLayout->setContentsMargins(0, 0, 0, 0);
         horizontalLayout = new QHBoxLayout();
         horizontalLayout->setSpacing(6);
         horizontalLayout->setObjectName(QString::fromUtf8("horizontalLayout"));
-        pTimerButton = new QPushButton(verticalLayoutWidget);
+        pTimerButton = new QPushButton(DialCient);
         pTimerButton->setObjectName(QString::fromUtf8("pTimerButton"));
         pTimerButton->setCheckable(true);
 
         horizontalLayout->addWidget(pTimerButton);
 
-        pQuitButton = new QPushButton(verticalLayoutWidget);
+        pQuitButton = new QPushButton(DialCient);
         pQuitButton->setObjectName(QString::fromUtf8("pQuitButton"));
 
         horizontalLayout->addWidget(pQuitButton);
 
-        horizontalLayout->setStretch(0, 4);
-        horizontalLayout->setStretch(1, 1);
+        horizontalLayout->setStretch(0, 7);
+        horizontalLayout->setStretch(1, 3);
 
         verticalLayout->addLayout(horizontalLayout);
 
@@ -85,49 +85,57 @@ public:
         horizontalLayout_2->setSpacing(0);
         horizontalLayout_2->setObjectName(QString::fromUtf8("horizontalLayout_2"));
         horizontalLayout_2->setContentsMargins(10, 10, -1, -1);
-        pLabel = new QLabel(verticalLayoutWidget);
+        pLabel = new QLabel(DialCient);
         pLabel->setObjectName(QString::fromUtf8("pLabel"));
 
         horizontalLayout_2->addWidget(pLabel);
 
-        horizontalSpacer = new QSpacerItem(40, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
-
-        horizontalLayout_2->addItem(horizontalSpacer);
-
-        pComboBox = new QComboBox(verticalLayoutWidget);
+        pComboBox = new QComboBox(DialCient);
         pComboBox->setObjectName(QString::fromUtf8("pComboBox"));
         pComboBox->setMaxCount(2147483644);
 
         horizontalLayout_2->addWidget(pComboBox);
 
+        horizontalLayout_2->setStretch(0, 7);
+        horizontalLayout_2->setStretch(1, 3);
 
         verticalLayout->addLayout(horizontalLayout_2);
 
         horizontalLayout_3 = new QHBoxLayout();
         horizontalLayout_3->setSpacing(6);
         horizontalLayout_3->setObjectName(QString::fromUtf8("horizontalLayout_3"));
-        pDial = new QDial(verticalLayoutWidget);
+        verticalLayout_3 = new QVBoxLayout();
+        verticalLayout_3->setSpacing(6);
+        verticalLayout_3->setObjectName(QString::fromUtf8("verticalLayout_3"));
+        pDial = new QDial(DialCient);
         pDial->setObjectName(QString::fromUtf8("pDial"));
         pDial->setMaximum(10);
         pDial->setSingleStep(1);
         pDial->setNotchesVisible(true);
 
-        horizontalLayout_3->addWidget(pDial);
+        verticalLayout_3->addWidget(pDial);
+
+
+        horizontalLayout_3->addLayout(verticalLayout_3);
 
         verticalLayout_2 = new QVBoxLayout();
         verticalLayout_2->setSpacing(0);
         verticalLayout_2->setObjectName(QString::fromUtf8("verticalLayout_2"));
         verticalLayout_2->setContentsMargins(-1, 35, -1, 35);
-        pArdLedBtn = new QPushButton(verticalLayoutWidget);
+        pArdLedBtn = new QPushButton(DialCient);
         pArdLedBtn->setObjectName(QString::fromUtf8("pArdLedBtn"));
         pArdLedBtn->setCheckable(true);
 
         verticalLayout_2->addWidget(pArdLedBtn);
 
-        pushButton_2 = new QPushButton(verticalLayoutWidget);
+        pushButton_2 = new QPushButton(DialCient);
         pushButton_2->setObjectName(QString::fromUtf8("pushButton_2"));
 
         verticalLayout_2->addWidget(pushButton_2);
+
+        verticalSpacer = new QSpacerItem(20, 40, QSizePolicy::Minimum, QSizePolicy::Expanding);
+
+        verticalLayout_2->addItem(verticalSpacer);
 
 
         horizontalLayout_3->addLayout(verticalLayout_2);
@@ -140,19 +148,21 @@ public:
         horizontalLayout_4 = new QHBoxLayout();
         horizontalLayout_4->setSpacing(6);
         horizontalLayout_4->setObjectName(QString::fromUtf8("horizontalLayout_4"));
-        pProgressBar = new QProgressBar(verticalLayoutWidget);
+        pProgressBar = new QProgressBar(DialCient);
         pProgressBar->setObjectName(QString::fromUtf8("pProgressBar"));
         pProgressBar->setValue(0);
 
         horizontalLayout_4->addWidget(pProgressBar);
 
-        pLcdNumber = new QLCDNumber(verticalLayoutWidget);
+        pLcdNumber = new QLCDNumber(DialCient);
         pLcdNumber->setObjectName(QString::fromUtf8("pLcdNumber"));
         pLcdNumber->setDigitCount(2);
         pLcdNumber->setSegmentStyle(QLCDNumber::Flat);
 
         horizontalLayout_4->addWidget(pLcdNumber);
 
+        horizontalLayout_4->setStretch(0, 7);
+        horizontalLayout_4->setStretch(1, 3);
 
         verticalLayout->addLayout(horizontalLayout_4);
 
@@ -160,6 +170,9 @@ public:
         verticalLayout->setStretch(1, 1);
         verticalLayout->setStretch(2, 5);
         verticalLayout->setStretch(3, 1);
+
+        verticalLayout_4->addLayout(verticalLayout);
+
 
         retranslateUi(DialCient);
         QObject::connect(pDial, SIGNAL(valueChanged(int)), pLcdNumber, SLOT(display(int)));
